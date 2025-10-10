8b6cbde24d05be6e5f18bfe26222004b6b479f98
KinnariyaMamaTanha <2823324228@qq.com>
2025-10-10 18:11:07 +0800

    deploy on github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroIcon from 'astro-icon';
 import playformCompress from "@playform/compress";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://KinnariyaMamaTanha.github.io",
+  base: "",
+  output: 'static',
   integrations: [
+    sitemap(),
     tailwind(),
     astroIcon({
       include: {
